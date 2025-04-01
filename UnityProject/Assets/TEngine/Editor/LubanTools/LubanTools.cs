30d5fbf8f987266cff31ad5bbf0a9d0201fb21ee
liruirui <liruirui@huixuanjiasu.com>
2025-04-01 21:11:54 +0800

    [feat]Unity中支持生成proto工具栏

--- UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
+++ UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
@@ -16,5 +16,17 @@ namespace TEngine.Editor
             Debug.Log($"执行转表：{path}");
             ShellHelper.RunByPath(path);
         }
+        
+        [MenuItem("TEngine/Luban/生成协议", priority = -101)]
+        private static void BuildProto()
+        {
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
+            string path = Application.dataPath + "/../../LubanTools/Proto/build_proto_to_client.sh";
+#elif UNITY_EDITOR_WIN
+            string path = Application.dataPath + "/../../LubanTools/Proto/build_proto_to_client.bat";
+#endif
+            Debug.Log($"生成proto：{path}");
+            ShellHelper.RunByPath(path);
+        }
     }
 }

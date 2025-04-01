9fdecc6e93c952a66d5a302a992d3e21622ab80d
liruirui <liruirui@huixuanjiasu.com>
2025-04-01 19:22:10 +0800

    [feat]Unity中可以使用导表工具

--- UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
+++ UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
@@ -5,13 +5,13 @@ namespace TEngine.Editor
 {
     public static class LubanTools
     {
-        [MenuItem("TEngine/Luban/转表 &X", priority = -100)]
+        [MenuItem("TEngine/Luban/转表", priority = -100)]
         private static void ZhuanXiaoYi()
         {
 #if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
-            string path = Application.dataPath + "/../../Configs/GameConfig/gen_code_bin_to_project_lazyload.sh";
+            string path = Application.dataPath + "/../../LubanTools/Configs/build_config_to_client.sh";
 #elif UNITY_EDITOR_WIN
-            string path = Application.dataPath + "/../../Configs/GameConfig/gen_code_bin_to_project_lazyload.bat";
+            string path = Application.dataPath + "/../../LubanTools/Configs/build_config_to_client.bat";
 #endif
             Debug.Log($"执行转表：{path}");
             ShellHelper.RunByPath(path);
